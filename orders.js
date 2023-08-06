35ce440d6fa1d493f9230ed0907b0f9c9e3d17af
Hamid Nawaz <138748939+haminawaz@users.noreply.github.com>
2023-08-06 22:43:17 +0500

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Pending',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Larvender KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName : 'Ruko F11 Pro Drone',
+        productNumber: '293992',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Camera Drone',
+        productNumber: '72982',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName : 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName : 'DJI Air S2',
+        productNumber: '81473',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Lozenge Drone',
+        productNumber: '00483',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
